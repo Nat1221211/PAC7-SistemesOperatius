e5dcc1aa2fe9ccab9da10bf9d638ffd9d45c1e02
Nat1221211 <bpuig@xarxa.fedac.cat>
2026-03-18 16:45:59 +0100

    script bash 1

--- Bash_Scripts/SystemInfo_Script/SystemInfo.sh
+++ Bash_Scripts/SystemInfo_Script/SystemInfo.sh
@@ -1,1 +1,23 @@
-a
+#!/bin/bash
+
+echo "Machine Name"
+sudo hostname
+echo
+echo "Kernel Version"
+uname -r
+echo
+echo "Kernel Name"
+uname -s
+echo
+echo "Hardware Architecture"
+uname -m
+echo
+echo "Linux Version"
+lsb_release -a
+echo
+echo "Hardware Info"
+lshw
+echo
+echo "Ethernet Info"
+ip addr show
+
