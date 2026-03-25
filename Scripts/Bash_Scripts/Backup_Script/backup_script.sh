ec4d595c3627975d9e13b4f36843f80e1487f572
Nat1221211 <bpuig@xarxa.fedac.cat>
2026-03-25 18:31:27 +0100

    Daily Backup

--- Scripts/Bash_Scripts/Backup_Script/backup_script.sh
+++ Scripts/Bash_Scripts/Backup_Script/backup_script.sh
@@ -7,6 +7,8 @@ cd "$DIR"
 
 tar -cpzf ./Backups/backup.tar.gz --one-file-system /etc
 
+echo "Backup Realized at $(date '+%Y-%m-%d %H:%M:%S')" >> ./backup.log
+
 OWNER=$(stat -c '%U' "$DIR")
 
 su -l "$OWNER" -c "cd $DIR; git add -A; git commit -m 'Daily Backup'; git push"
