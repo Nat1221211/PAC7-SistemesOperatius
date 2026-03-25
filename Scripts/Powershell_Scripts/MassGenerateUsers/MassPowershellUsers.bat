8bcce85497d1ea4826aa0ccc948e8cb14d6d112f
Nat1221211 <bpuig@xarxa.fedac.cat>
2026-03-25 17:40:55 +0100

    Powershell Users FUnctional By CSV

--- Scripts/Powershell_Scripts/MassGenerateUsers/MassPowershellUsers.bat
+++ Scripts/Powershell_Scripts/MassGenerateUsers/MassPowershellUsers.bat
@@ -1,3 +1,3 @@
 @echo off
 
-powershell -ExecutionPolicy Bypass -Command "Import-Csv 'Users.csv' | ForEachObject {New-LocalUser -Name $_.Name -Password (ConvertTo-SecureString $_.pass -AsPlainText -Force) -Description "CSv User"}"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& { $csv = Import-Csv '%~dp0Users.csv'; foreach ($u in $csv) {$passw = $u.pass; $pass = ConvertTo-SecureString $passw -AsPlainText -Force; $nombre = $u.name; New-LocalUser -Name $nombre -Password $pass -Description 'CSv User'}}"
